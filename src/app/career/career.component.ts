25a208fc6f9c32ef791b57c6915fdf86273693f8
andiandiandi <andy.libert@hotmail.de>
2021-10-11 16:25:31 +0200

    add faq

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { DataService, Job } from '../shared/data.service';
 
+interface Faq {
+  question: string;
+  answer: string;
+  expanded: boolean;
+}
+
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
@@ -16,7 +22,14 @@ export class CareerComponent implements OnInit {
 
   companyImages: { path: string }[] = [];
 
+  faq: Faq[] = [];
+
   constructor(private dataService: DataService) {}
+
+  toggleExpanded(item: Faq) {
+    this.faq.forEach((i) => (i.expanded = false));
+    item.expanded = true;
+  }
 
   filterJobs(target: any, selectOption: string) {
     if (selectOption == 'department') this.selectedDepartment = target.value;
@@ -37,6 +50,35 @@ export class CareerComponent implements OnInit {
       },
       {
         path: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.80000hours.org%2Fwp-content%2Fuploads%2F2015%2F07%2Fworking2-fullres-100031101-orig.jpg&f=1&nofb=1',
+      },
+    ];
+
+    this.faq = [
+      {
+        question: 'Can I work from home',
+        answer:
+          'Yes! You can decide if you want to work in the office or from home.',
+        expanded: false,
+      },
+      {
+        question: 'Can i work remotely',
+        answer:
+          'Unless explicitly stated in the position’s location, most of our positions are available to work from remote.',
+        expanded: false,
+      },
+      {
+        question: 'What will my working hours look like',
+        answer:
+          'Productive hours differ individually. That’s why you are welcome to schedule your working hours as works best for you. However, to respect everybody’s freedom to decide when their most productive times are, we consider our core hours to be Monday to Friday, between 10:30am and 4:00pm OR whenever you have team meetings.',
+        expanded: false,
+      },
+      {
+        question: 'Can I bring my family',
+        answer:
+          'If you are an EU-citizen or eligible for a Blue Card this process is easy, there should be no problems.\n' +
+          'If you are in none of these categories the process involves more steps, nevertheless we will assist you to bring your loved ones as soon as possible.\n' +
+          'For detailed information see: https://www.berlin.de/willkommenszentrum/en/families/reunification-of-families/',
+        expanded: false,
       },
     ];
   }
